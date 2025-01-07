5bd5e15e54f198930c3c96cfb8f2eb6706d5062c
Sivaji Kondapalli <sivaji.kondapalli@ianwilliams.co.uk>
2025-01-07 18:11:32 +0000

    Prob202

---
+++ src/main/java/lc/easy/Prob0202_HappyNumber.java
@@ -0,0 +1,11 @@
+package lc.easy;
+
+public class Prob0202_HappyNumber {
+    public static void main(String[] args) {
+
+    }
+
+    static public boolean isHappy(int n) {
+        return false;
+    }
+}
