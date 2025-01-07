9212aa4f19cafe01f60b3a1f76dfbabd4e8f491a
Sivaji Kondapalli <sivaji.kondapalli@ianwilliams.co.uk>
2025-01-07 19:17:33 +0000

    Prob202

--- src/main/java/lc/easy/Prob0202_HappyNumber.java
+++ src/main/java/lc/easy/Prob0202_HappyNumber.java
@@ -1,11 +1,34 @@
 package lc.easy;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Prob0202_HappyNumber {
     public static void main(String[] args) {
-
+        System.out.println(isHappy(199));
     }
 
     static public boolean isHappy(int n) {
-        return false;
+        Set<Integer> set = new HashSet<>();
+        while (true) {
+            n = getSumOfDigits(n);
+            if (n == 1) {
+                return true;
+            }
+            if (set.contains(n)) {
+                return false;
+            }
+            set.add(n);
+        }
+    }
+
+    static int getSumOfDigits(int n) {
+        int sum = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            sum += (digit * digit);
+            n = n / 10;
+        }
+        return sum;
     }
 }
