ecbe6c23c46985ecf00851d3db69bb03d12c1ec8
sivajik <sivaji_kondapalli@yahoo.com>
2025-04-29 20:09:47 +0100

    Prob125 To Start with.

---
+++ src/main/java/com/lc2dot0/easy/Prob0125_ValidPalindrome.java
@@ -0,0 +1,11 @@
+package com.lc2dot0.easy;
+
+public class Prob0125_ValidPalindrome {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean isPalindrome(String s) {
+        return false;
+    }
+}
