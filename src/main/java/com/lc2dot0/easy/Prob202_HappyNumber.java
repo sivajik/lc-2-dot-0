0a5e332d17b1ea4fc8f16745aeafddd0505f6ec5
sivajik <sivaji_kondapalli@yahoo.com>
2025-04-30 16:08:40 +0100

    Prob202 Happy Number

---
+++ src/main/java/com/lc2dot0/easy/Prob202_HappyNumber.java
@@ -0,0 +1,36 @@
+package com.lc2dot0.easy;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Prob202_HappyNumber {
+    public static void main(String[] args) {
+        System.out.println(isHappy(19) == true);
+        System.out.println(isHappy(2) == false);
+        System.out.println(isHappy(1) == true);
+    }
+
+    public static boolean isHappy(int n) {
+        Set<Integer> set = new HashSet<>();
+        set.add(n);
+
+        while (n != 1) {
+            int sum = digitSum(n);
+            if (set.contains(sum)) {
+                return false;
+            }
+            set.add(sum);
+            n = sum;
+        }
+        return true;
+    }
+
+    private static int digitSum(int n) {
+        int sum = 0;
+        while (n > 0) {
+            sum += ((n % 10) * (n % 10));
+            n = n / 10;
+        }
+        return sum;
+    }
+}
