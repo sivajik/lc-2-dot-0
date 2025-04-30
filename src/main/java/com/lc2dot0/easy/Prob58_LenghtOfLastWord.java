c564cff76f3ef0706c4fb5b71eda39957ba4fff5
sivajik <sivaji_kondapalli@yahoo.com>
2025-04-30 21:01:19 +0100

    better and 100% solution

--- src/main/java/com/lc2dot0/easy/Prob58_LenghtOfLastWord.java
+++ src/main/java/com/lc2dot0/easy/Prob58_LenghtOfLastWord.java
@@ -16,8 +16,20 @@ public class Prob58_LenghtOfLastWord {
         /*String[] tokens = s.split(" ");
         return tokens[tokens.length - 1].length();*/
 
-        String[] tokens = split(s);
-        return tokens[tokens.length - 1].length();
+        /*String[] tokens = split(s);
+        return tokens[tokens.length - 1].length();*/
+
+        String str = s.trim();
+        char[] chars = str.toCharArray();
+        int count = 0;
+        for (int i = chars.length-1; i >= 0; i--) {
+            if (chars[i] != ' ') {
+                count++;
+            } else {
+                break;
+            }
+        }
+        return count;
     }
 
     private static String[] split(String str) {
