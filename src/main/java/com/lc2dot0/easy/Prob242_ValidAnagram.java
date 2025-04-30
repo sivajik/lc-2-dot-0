decb0ecd123109a259efb0770ee315d9dc0d0ba3
sivajik <sivaji_kondapalli@yahoo.com>
2025-04-30 12:34:00 +0100

    Prob 242 Valid Anagram

---
+++ src/main/java/com/lc2dot0/easy/Prob242_ValidAnagram.java
@@ -0,0 +1,35 @@
+package com.lc2dot0.easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Prob242_ValidAnagram {
+    public static void main(String[] args) {
+        System.out.println(isAnagram("anagram", "nagaram") == true);
+        System.out.println(isAnagram("rat", "car") == false);
+    }
+
+    public static boolean isAnagram(String s, String t) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            if (map.containsKey(c)) {
+                map.put(c, 1 + map.get(c));
+            } else {
+                map.put(c, 1);
+            }
+        }
+
+        for (char c : t.toCharArray()) {
+            if (!map.containsKey(c)) {
+                return false;
+            } else {
+                if (map.get(c) == 1) {
+                    map.remove(c);
+                } else if (map.get(c) > 1) {
+                    map.put(c, map.get(c) - 1);
+                }
+            }
+        }
+        return map.isEmpty();
+    }
+}
