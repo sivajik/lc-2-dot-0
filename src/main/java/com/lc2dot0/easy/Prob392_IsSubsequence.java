1f69e91ce701e17883a3e40272d0ff1bd7941079
sivajik <sivaji_kondapalli@yahoo.com>
2025-05-06 16:41:05 +0100

    Prob392 IsSubsequence

---
+++ src/main/java/com/lc2dot0/easy/Prob392_IsSubsequence.java
@@ -0,0 +1,26 @@
+package com.lc2dot0.easy;
+
+public class Prob392_IsSubsequence {
+    public static void main(String[] args) {
+        System.out.println(isSubsequence("abc", "ahbgdc"));
+        System.out.println(isSubsequence("axc", "ahbgdc"));
+    }
+
+    public static boolean isSubsequence(String s, String t) {
+        int s1 = 0;
+        int t1 = 0;
+
+        while (s1 < s.length() && t1 < t.length()) {
+            char s2 = s.charAt(s1);
+            char t2 = t.charAt(t1);
+
+            if (s2 == t2) {
+                s1++;
+                t1++;
+            } else {
+                t1++;
+            }
+        }
+        return s1 == s.length() && t1 <= t.length();
+    }
+}
