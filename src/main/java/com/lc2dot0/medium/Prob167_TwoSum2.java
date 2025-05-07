ce5957ca3bdad794e37b33d0545b3a56a97fa10c
sivajik <sivaji_kondapalli@yahoo.com>
2025-05-07 09:31:29 +0100

    Prob167 TwoSum medium

---
+++ src/main/java/com/lc2dot0/medium/Prob167_TwoSum2.java
@@ -0,0 +1,26 @@
+package com.lc2dot0.medium;
+
+import java.util.Arrays;
+
+public class Prob167_TwoSum2 {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(twoSum(new int[]{2, 7, 11, 15}, 9)));
+        System.out.println(Arrays.toString(twoSum(new int[]{2, 3, 4}, 6)));
+        System.out.println(Arrays.toString(twoSum(new int[]{-1, 0}, -1)));
+    }
+
+    public static int[] twoSum(int[] numbers, int target) {
+        int l = 0;
+        int r = numbers.length - 1;
+        while (l < r) {
+            if (numbers[l] + numbers[r] == target) {
+                return new int[]{l + 1, r + 1};
+            } else if (numbers[l] + numbers[r] < target) {
+                l++;
+            } else {
+                r--;
+            }
+        }
+        return new int[]{-1, -1};
+    }
+}
