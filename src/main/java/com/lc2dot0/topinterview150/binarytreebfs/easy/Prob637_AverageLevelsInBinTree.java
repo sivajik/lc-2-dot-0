0a4e29af3936ab31e759f5c4016d38aabf84e3df
sivajik <sivaji_kondapalli@yahoo.com>
2025-05-08 09:59:47 +0100

    Prob637 AverageLevelsInBinTree

---
+++ src/main/java/com/lc2dot0/topinterview150/binarytreebfs/easy/Prob637_AverageLevelsInBinTree.java
@@ -0,0 +1,86 @@
+package com.lc2dot0.topinterview150.binarytreebfs.easy;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class Prob637_AverageLevelsInBinTree {
+    public static void main(String[] args) {
+        TreeNode n1 = prepareTree(new int[]{3, 9, 20, -999, -999, 15, 7});
+        List<Double> l = averageOfLevels(n1);
+        System.out.println(l);
+    }
+
+    public static List<Double> averageOfLevels(TreeNode root) {
+        List<Double> result = new ArrayList<>();
+        Queue<TreeNode> q = new LinkedList<>();
+
+        q.add(root);
+        while (!q.isEmpty()) {
+            int len = q.size();
+            double sum = 0;
+            for (int i = 0; i < len; i++) {
+                TreeNode node = q.poll();
+                sum += node.val;
+
+                if (node.left != null) {
+                    q.offer(node.left);
+                }
+                if (node.right != null) {
+                    q.offer(node.right);
+                }
+
+            }
+            result.add(sum / len);
+        }
+        return result;
+    }
+
+    public static TreeNode prepareTree(int[] values) {
+        TreeNode[] treeArray = new TreeNode[values.length];
+        for (int i = 0; i < values.length; i++) {
+            if (values[i] == -999) {
+                treeArray[i] = null;
+            } else {
+                treeArray[i] = new TreeNode(values[i]);
+            }
+        }
+
+        for (int i = 0; i < values.length; i++) {
+            TreeNode node = treeArray[i];
+            if (((2 * i) + 1 < values.length) && ((2 * i) + 2 < values.length)) {
+                node.left = treeArray[(2 * i) + 1];
+                node.right = treeArray[(2 * i) + 2];
+            }
+        }
+        return treeArray[0];
+    }
+
+    public static void inOrder(TreeNode tree) {
+        if (tree != null) {
+            inOrder(tree.left);
+            System.out.print(tree.val + " ");
+            inOrder(tree.right);
+        }
+    }
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
