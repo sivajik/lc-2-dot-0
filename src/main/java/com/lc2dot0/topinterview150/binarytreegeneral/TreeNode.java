a625cb2d4230a3f09dda031fe5e9954a9840105d
sivajik <sivaji_kondapalli@yahoo.com>
2025-05-09 09:45:29 +0100

    Prob104 MaxDepthOfBinaryTree

---
+++ src/main/java/com/lc2dot0/topinterview150/binarytreegeneral/TreeNode.java
@@ -0,0 +1,17 @@
+package com.lc2dot0.topinterview150.binarytreegeneral;
+
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
---
+++ src/main/java/com/lc2dot0/topinterview150/binarytreegeneral/easy/Prob104_MaxDepthOfBinaryTree.java
@@ -0,0 +1,19 @@
+package com.lc2dot0.topinterview150.binarytreegeneral.easy;
+
+import com.lc2dot0.topinterview150.binarytreegeneral.TreeNode;
+
+import static com.lc2dot0.topinterview150.binarytreegeneral.TreeNodeUtils.prepareTree;
+
+public class Prob104_MaxDepthOfBinaryTree {
+    public static void main(String[] args) {
+        TreeNode root = prepareTree(new int[]{3, 9, 20, -999, -999, 15, 7});
+        System.out.println(maxDepth(root));
+    }
+
+    public static int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+}
