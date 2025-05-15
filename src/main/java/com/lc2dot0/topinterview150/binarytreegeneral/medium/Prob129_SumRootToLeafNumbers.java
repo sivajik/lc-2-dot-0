07f1beeb841b2bf51382ab3cb7a6b18da9cae9f0
sivajik <sivaji_kondapalli@yahoo.com>
2025-05-15 12:24:41 +0100

    Prob129_SumRootToLeafNumbers

--- src/main/java/com/lc2dot0/topinterview150/binarytreegeneral/medium/Prob129_SumRootToLeafNumbers.java
+++ src/main/java/com/lc2dot0/topinterview150/binarytreegeneral/medium/Prob129_SumRootToLeafNumbers.java
@@ -13,10 +13,7 @@ public class Prob129_SumRootToLeafNumbers {
         TreeNode t = TreeNodeUtils.prepareTree(new int[]{4, 9, 0, 5, 1, -999, -999});
         inOrder(t);
         System.out.println();
-        //System.out.println(sumNumbers(t));
-        for (var x : printPaths(t)) {
-            System.out.println(x);
-        }
+        System.out.println(sumNumbers(t));
     }
 
     public static int sumNumbers(TreeNode root) {
@@ -34,21 +31,5 @@ public class Prob129_SumRootToLeafNumbers {
         int l = sum(root.left, root.val + (10 * multipler));
         int r = sum(root.right, root.val + (10 * multipler));
         return l + r;
-    }
-
-    private static List<Integer> printPaths(TreeNode root) {
-        List<Integer> list = new ArrayList<>();
-        print(root, list);
-        return list;
-    }
-
-    private static void print(TreeNode root, List<Integer> list) {
-        if (root != null) {
-            if (root.left == null && root.right == null) {
-                list.add(root.val);
-            }
-            print(root.left, list);
-            print(root.right, list);
-        }
     }
 }
